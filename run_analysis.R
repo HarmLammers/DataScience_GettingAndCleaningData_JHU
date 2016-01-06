9f63cf305f7a198c12d8e4f789b68e56cfdf15cf
Harm Lammers <HarmLammers@ziggo.nl>
2016-01-06 14:42:58 +0100

    Local issues globalised
    Local settings removed to make it work for others in the workdirectory,
    Temporary results will not be shown
    Comments adjusted

--- run_analysis.R
+++ run_analysis.R
@@ -28,11 +28,10 @@
 ##	
 ##	Good luck!
 ##
-##	The datasets have been download manually into dir GaCDCourseProject in my workdirectory & subsequently unzipped
-##	I.ve removed the spaces in the directory-names manually
-##
-##	I.ve a slight preference to keep instructions in a general form and to 
-##	use variables to steer the individual cases
+# 	The datasets have been downloaded manually into dir GaCDCourseProject & subsequently unzipped to my workdirectory
+# 
+# 	I.ve a slight preference to keep instructions in a general form and to 
+# 	use variables to steer the individual cases
 ##
 ##	You should create one R script called run_analysis.R that does the following. 
 ##	1. Merges the training and the test sets to create one data set.
@@ -40,29 +39,29 @@
 ##	Measurements
 ##	The original dataset was split into a trainingset and a testset
 ## 	Combine them together (rows)
-trainfn 		<- ("./GaCDCourseProject/UCIHARDataset/train/X_train.txt")		## name file - train
-testfn 		<- ("./GaCDCourseProject/UCIHARDataset/test/X_test.txt")		## name file - test
-xtraindata		<- read.table(trainfn)								## read file into a dataframe-variable
-xtestdata		<- read.table(testfn)								## read file into a dataframe-variable
-measurementdata	<- rbind(xtraindata, xtestdata)						## bind the two dataframes
+trainfn 				<- ("./train/X_train.txt")							## name file - train
+testfn 				<- ("./test/X_test.txt")							## name file - test
+xtraindata				<- read.table(trainfn)								## read file into a dataframe-variable
+xtestdata				<- read.table(testfn)								## read file into a dataframe-variable
+measurementdata			<- rbind(xtraindata, xtestdata)						## bind the two dataframes
 
 ##	Activities 
 ##	The original dataset was split into a trainingset and a testset
 ## 	Combine them together (rows)
-trainfn 		<- ("./GaCDCourseProject/UCIHARDataset/train/y_train.txt")
-testfn 		<- ("./GaCDCourseProject/UCIHARDataset/test/y_test.txt")
-ytraindata		<- read.table(trainfn)
-ytestdata		<- read.table(testfn)
-activitydata	<- rbind(ytraindata, ytestdata)
+trainfn 				<- ("./train/y_train.txt")
+testfn 				<- ("./test/y_test.txt")
+ytraindata				<- read.table(trainfn)
+ytestdata				<- read.table(testfn)
+activitydata			<- rbind(ytraindata, ytestdata)
 
 ##	Subjects
 ##	The original dataset was split into a trainingset and a testset
 ## 	Combine them together (rows)
-trainfn 		<- ("./GaCDCourseProject/UCIHARDataset/train/subject_train.txt")
-testfn 		<- ("./GaCDCourseProject/UCIHARDataset/test/subject_test.txt")
-straindata		<- read.table(trainfn)
-stestdata		<- read.table(testfn)
-subjectdata		<- rbind(straindata, stestdata)
+trainfn 				<- ("./train/subject_train.txt")
+testfn 				<- ("./test/subject_test.txt")
+straindata				<- read.table(trainfn)
+stestdata				<- read.table(testfn)
+subjectdata				<- rbind(straindata, stestdata)
 
 ##	We stil have 3 datasets here, but the cleaning (as asked for) will be done on every set seperately
 ##	Which leaves us the task to combine the datasets columnwise afterwards
@@ -71,33 +70,32 @@ subjectdata		<- rbind(straindata, stestdata)
 ##	D. Merges the training and the test sets to create one data set.
 ##	2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 
-features 		<- read.table("./GaCDCourseProject/UCIHARDataset/features.txt")	## Read features into a data.frame
-usedFeatures 	<- features[grep("-mean\\(\\)|-std\\(\\)",features[,2]), ]		## select features containing -mean() or -std()
-														## use escape-characters to identify the parenthesis characters
-measurement_meanstd		<- measurementdata[ , usedFeatures[ ,1]]			## select columns corresponding to the selected features
+featuredata				<- read.table("features.txt")							## Read features into a data.frame
+usedFeatures 			<- features[grep("-mean\\(\\)|-std\\(\\)",featuredata[,2]), ]	## select features containing -mean() or -std()
+																## use escape-characters to identify the parenthesis characters
+measurement_meanstd		<- measurementdata[ , usedFeatures[ ,1]]					## select columns corresponding to the selected features
 
 ##	You should create one R script called run_analysis.R that does the following. 
 ##	D. Merges the training and the test sets to create one data set.
 ##	D. Extracts only the measurements on the mean and standard deviation for each measurement. 
 ##	3. Uses descriptive activity names to name the activities in the data set
 
-activitylabeldata			<- read.table("./GaCDCourseProject/UCIHARDataset/activity_labels.txt")	## Read activity labels into a data.frame
-names(activitylabeldata)	<- c("activitycode", "activitylabel")						## assign meaningfull column names, lowercase preferably
-activitylabeldata[, 2]		<- gsub("_", "-", tolower(activitylabeldata[ ,2]))				## restyle labels to lowercase and use readable dashes
-
-activity				<- activitylabeldata[activitydata[,1], 2]			## replace numbers by corresponding labels
-activity													## check the result
+activitylabeldata			<- read.table("activity_labels.txt")					## Read activity labels into a data.frame
+names(activitylabeldata)	<- c("activitycode", "activitylabel")					## assign meaningfull column names, lowercase preferably
+activitylabeldata[, 2]		<- gsub("_", "-", tolower(activitylabeldata[ ,2]))			## restyle labels to lowercase and use readable dashes
+	
+activity				<- activitylabeldata[activitydata[,1], 2]					## replace numbers by corresponding labels
+## activity															## check the result
 
 ##	4. Appropriately labels the data set with descriptive variable names. 
-names(measurement_meanstd)										## reveals non-descriptive Vxx names
-names(measurement_meanstd)	<- tolower(usedFeatures[ , 2])				## copy feature names from usedFeatures
-														## set them to lowercase
-names(measurement_meanstd)	<- gsub("\\(|\\)","",names(xdata_meanstd))		## get rid of the pointless parentheses in the column names
-names(measurement_meanstd)										## check the result
+## names(measurement_meanstd)												## reveals non-descriptive Vxx names
+names(measurement_meanstd)	<- tolower(usedFeatures[ , 2])						## copy feature names from usedFeatures
+																## set them to lowercase
+names(measurement_meanstd)	<- gsub("\\(|\\)","",names(measurement_meanstd))			## get rid of the pointless parentheses in the column names
+## names(measurement_meanstd)												## check the result
 
-names(activity)			<- "activity"
-
-names(subjectdata) 		<- "subject"
+names(activity)			<- "activity"									## assign descriptive column name 
+names(subjectdata) 		<- "subject"									## assign descriptive column name
 
 ##	You should create one R script called run_analysis.R that does the following. 
 ##	1. Merges the training and the test sets to create one data set - REVISITED
@@ -105,8 +103,8 @@ names(subjectdata) 		<- "subject"
 ##	We stil have 3 datasets here, but the cleaning (as asked for) has been done on every set seperately
 ##	Which leaves us the task to combine the datasets columnwise now
 
-projectdata				<- cbind(subjectdata, activity, measurement_meanstd)
-write.table(projectdata, "merged_cleansed_projectdata.txt", row.names=FALSE)
+projectdata				<- cbind(subjectdata, activity, measurement_meanstd)			## bind datasets together, column wise, identifiers first
+write.table(projectdata, "merged_cleansed_projectdata.txt", row.names=FALSE)				## keep result of the work for later use
 
 
 ##	You should create one R script called run_analysis.R that does the following. 
@@ -118,10 +116,12 @@ write.table(projectdata, "merged_cleansed_projectdata.txt", row.names=FALSE)
 ##	   with the average of each variable for each activity and each subject.
 ##	
 
+##	use aggregate function; identify columns (without identifiers) to aggregate, identify the group-by columns in a list, calculate the mean 
 agg		<- aggregate(projectdata[, 3:dim(projectdata)[2]], list(projectdata$subject, projectdata$activity), mean)
-names(agg)	<- names(projectdata)						## aggregation columns have been renamed
+names(agg)	<- names(projectdata)											## aggregation/group_by columns have been renamed to groupx
 write.table(agg, "merged_cleansed_projectdata_avg_aggregate.txt", row.names=FALSE)
 
+##	use package dplyr as mentioned in the video lectures to create an alternative result
 library(dplyr)
 effe	<- projectdata %>% group_by(subject, activity) %>% summarise_each(funs(mean(., na.rm=TRUE)))
 write.table(effe, "merged_cleansed_projectdata_avg_dplyr.txt", row.names=FALSE)
